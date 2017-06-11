4128c555dc2d0d98a3836df893fb02abf47d01a0
inuscript <inuscript@users.noreply.github.com>
2017-06-11 18:09:12 +0900

    wip

--- api.sh
+++ api.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -x
 
 # 前回実行からstatusを採る
-[ $? = "0" ] && RESULT="success" || RESULT="failure"
+[ $? = "0" ] && STATUS="success" || STATUS="failure"
+TITLE=$1
 
-post(){
-  if [ ! ${CI} ]; then
-    exit
-  fi
+if [ ! ${CI} ]; then
+  exit
+fi
 
-  BASE_URL=https://api.github.com
-  URL=${BASE_URL}/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/statuses/${CIRCLE_SHA1}
-}
+BASE_URL=https://api.github.com
+URL=${BASE_URL}/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/statuses/${CIRCLE_SHA1}
 
-post(RESULT)
+curl ${BASE_URL} \
+  -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"state\": \"${STATUS}\", \"context\": \"${TITLE}\" }"
+
