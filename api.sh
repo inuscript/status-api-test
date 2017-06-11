4b7108eb529a85cf3581bf37b077d324b669f46a
inuscript <inuscript@users.noreply.github.com>
2017-06-11 18:00:34 +0900

    wip

--- api.sh
+++ api.sh
@@ -1,6 +1,15 @@
-#! /bin/bash
+#!/bin/bash -x
 
-RESULT = $? ? "failure" : "success"
-[!$CI] && echo "exit" && exit;
+# 前回実行からstatusを採る
+[ $? = "0" ] && RESULT="success" || RESULT="failure"
 
-URL=https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/statuses/$CIRCLE_SHA1
+post(){
+  if [ ! ${CI} ]; then
+    exit
+  fi
+
+  BASE_URL=https://api.github.com
+  URL=${BASE_URL}/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/statuses/${CIRCLE_SHA1}
+}
+
+post(RESULT)
