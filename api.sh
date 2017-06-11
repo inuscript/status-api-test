4befd20f25e63278aec209e04536217eaa6bac77
inuscript <inuscript@users.noreply.github.com>
2017-06-11 17:48:16 +0900

    wip

--- api.sh
+++ api.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+RESULT = $? ? "failure" : "success"
 [!$CI] && echo "exit" && exit;
 
 URL=https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/statuses/$CIRCLE_SHA1
